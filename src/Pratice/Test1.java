04a9d5e3f18685a277530caf6b7aa5c2aa41db07
Narayan Thakare <narayan111thakare@gmail.com>
2025-01-08 23:31:17 +0530

    20th commit

--- src/Pratice/Test1.java
+++ src/Pratice/Test1.java
@@ -7,9 +7,13 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner sc = new Scanner (System.in);
+
+
 		
 		System.out.println("Enter the no.");
 		int no = Integer.parseInt(sc.next());
+
+		
 		
 		
 		System.out.println(no+no);
